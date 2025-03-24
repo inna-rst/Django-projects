f370daf3bb19d59094c43563bf212d51a77b2608
Inna <inn@gmail.com>
2025-03-24 18:19:05 +0200

    l-39. Add notes templates, add notes.css and create index notes view

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,18 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render
 
+
+class Notes:
+    def __init__(self, note_id):
+        self.id = note_id
+        self.title="Заголовок заметки " + str(note_id)
+        self.content="Текст заметки " + str(note_id)
+
+notes_list=[]
+for i in range (1,11):
+    notes_list.append(Notes(i))
 
 def index(request):
-    return HttpResponse("Hello from Notes app.")
+    return render(request, "notes/notes_list.html", {"notes": notes_list})
