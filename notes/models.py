84a01cd7fb167d19becc6cabf032ba5ff0d961b7
Inna <inn@gmail.com>
2025-03-31 18:10:44 +0300

    40-l. Add Model notes, notes-list view, add admin notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Notes(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text  = models.TextField()
+    reminder = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
